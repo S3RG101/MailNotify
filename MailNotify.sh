53ae4d6e136b02f2ab20d9270b037cd43961702d
S3RG101 <72516965+S3RG101@users.noreply.github.com>
2021-12-21 10:28:22 -0600

    Create MailNotify.sh

---
+++ MailNotify.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Monitor file $1 for changes
+# Send an alert emai to $2 if file $1 changes
+# usage: file_change_mail_alert.sh /var/log/messages your.name@domain.com
+#
+if [ -z “$2” ]; then
+
+echo "Usage: file_change_mail_alert.sh "
+
+exit 1
+
+fi
+
+
+#if a inotifywait for this file is already running
+
+if [ $(ps aux | grep inotifywait | grep -c “$1” ) -gt 0 ]; then
+
+echo “A process monitoring the file $1 is already running: $(ps aux | grep inotifywait | grep “$1” )”;
+
+exit 1;
+
+fi
+
+
+#if inotifywait exists
+
+type -P inotifywait &>/dev/null || { echo "Error: This script requires inotifywait(http://wiki.github.com/rvoicilas/inotify-tools/) … apt-get install inotify-tools … " >&2; exit 1; }
+
+
+#if the file exists
+
+
+if [ -f $1 ]; then
+
+echo “Monitoring file $1 for changes - sending alerts to $2”
+
+
+while inotifywait -e modify -e attrib -e move -e delete $1 -o /root/audit.log; do
+
+sleep 1
+
+
+changes="$(tail -1 $1)"
+
+# changes="$(cat /root/audit.log)"
+	
+	python3 /names/SendMails3.py "Change in $1: $changes"
+	rm /root/audit.log
+	touch /root/audit.log		
+done
+
+else
+
+echo “Error: File $1 not found”
+
+fi
